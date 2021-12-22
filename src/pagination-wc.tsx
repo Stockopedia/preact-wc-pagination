3d4629aa7d92650880faf5a9b2c977f9ca2cde3f
Adam Poczatek <adam.p@stockopedia.com>
2021-12-22 17:56:55 +0000

    Added pagination web component

---
+++ src/pagination-wc.tsx
@@ -0,0 +1,31 @@
+import register from 'preact-custom-element';
+import {useRef} from "preact/hooks"
+import {StkPagination} from "./pagination";
+import {VNode} from "preact";
+
+function StkPaginationWC({ currentPage, totalPages }): VNode {
+  const ref = useRef(null);
+
+  // Events
+  const pageChange = (pageNumber: number) => {
+    const componentElement = ref.current.base.parentElement;
+    componentElement.dispatchEvent(new CustomEvent('pageChange', {
+      detail: { pageNumber }
+    }));
+  };
+
+  return (
+    <StkPagination
+      ref={ref}
+      currentPage={parseNumberProp(currentPage)}
+      totalPages={parseNumberProp(totalPages)}
+      onPageChange={pageChange}
+    />
+  )
+}
+
+function parseNumberProp(prop: string | number): number {
+  return typeof prop === 'string' ? parseInt(prop, 10) : prop;
+}
+
+register(StkPaginationWC, 'stk-pagination', ['current-page', 'total-pages', 'data'], { shadow: false });
---
+++ src/pagination.tsx
@@ -0,0 +1,157 @@
+import classNames from 'classnames'
+import {Fragment} from "preact";
+import {MutableRef} from "preact/hooks";
+
+export interface PaginationProps {
+  currentPage: number
+  totalPages: number
+  onPageChange: (pageNumber: number) => void
+  ref?: MutableRef<any>
+}
+
+interface PageData {
+  number?: number
+  selected?: boolean
+  separator: boolean
+}
+
+const PAGINATION_DELTA = 2
+const MAX_PAGES = 5
+
+export function StkPagination({ totalPages, currentPage, onPageChange }: PaginationProps) {
+  totalPages = totalPages || MAX_PAGES;
+
+  const buildPageNumber = (number: number): PageData => ({
+    number,
+    selected: number === currentPage,
+    separator: false,
+  })
+
+  const handlePageChange = (number: number) => {
+    onPageChange(number);
+  }
+
+  const generatePages = (): ReadonlyArray<PageData> => {
+    // Based on simple-pagination.js algorithm
+    // https://gist.github.com/kottenator/9d936eb3e4e3c3e02598
+    const last = totalPages
+    const left = currentPage - PAGINATION_DELTA
+    const right = currentPage + PAGINATION_DELTA + 1
+
+    const range = Array.from(new Array(last), (n, index) => index + 1).filter(
+      (page) => {
+        return page === 1 || page === last || (left <= page && page < right)
+      },
+    )
+
+    const results: PageData[] = []
+
+    let previousPage: number
+    range.forEach((page) => {
+      if (previousPage) {
+        if (page - previousPage === 2) {
+          results.push(buildPageNumber(previousPage + 1))
+        } else if (page - previousPage !== 1) {
+          results.push({ separator: true })
+        }
+      }
+      results.push(buildPageNumber(page))
+      previousPage = page
+    })
+
+    return results
+  }
+
+  const pages = generatePages()
+  const hasPreviousPage = 0 < currentPage - 1
+  const hasNextPage = currentPage + 1 <= totalPages
+
+  return (
+    <>
+      <nav className="stk-pagination">
+        {hasPreviousPage ? (
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            className={classNames(
+              'stk-pagination__button',
+              'stk-pagination__button--prev',
+              'stk-pagination__button--first',
+            )}
+          >
+            Previous
+          </button>
+        ) : (
+          <span
+            className={classNames(
+              'stk-pagination__button',
+              'stk-pagination__button--prev',
+              'stk-pagination__button--first',
+              'stk-pagination__button--disabled',
+            )}
+          >
+            Previous
+          </span>
+        )}
+        {pages.map((page, index) => (
+          <Fragment key={page}>
+            {page.number ? (
+              <button
+                onClick={() => handlePageChange(page.number)}
+                className={classNames('stk-pagination__button', {
+                  'stk-pagination__button--selected': page.selected,
+                  'stk-pagination__button--first': isSeparatorPrior(
+                    page,
+                    pages,
+                  ),
+                  'stk-pagination__button--last': isSeparatorAfter(page, pages),
+                })}
+                key={index}
+              >
+                {page.number}
+              </button>
+            ) : (
+              <span className="stk-pagination__separator">&hellip;</span>
+            )}
+          </Fragment>
+        ))}
+        {hasNextPage ? (
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            className={classNames(
+              'stk-pagination__button',
+              'stk-pagination__button--next',
+              'stk-pagination__button--last',
+            )}
+          >
+            Next
+          </button>
+        ) : (
+          <span
+            className={classNames(
+              'stk-pagination__button',
+              'stk-pagination__button--next',
+              'stk-pagination__button--last',
+              'stk-pagination__button--disabled',
+            )}
+          >
+            Next
+          </span>
+        )}
+      </nav>
+    </>
+  )
+}
+
+function isSeparatorPrior(
+  page: PageData,
+  pages: ReadonlyArray<PageData>,
+): boolean {
+  return pages[pages.indexOf(page) - 1]?.separator
+}
+
+function isSeparatorAfter(
+  page: PageData,
+  pages: ReadonlyArray<PageData>,
+): boolean {
+  return pages[pages.indexOf(page) + 1]?.separator
+}
